7685e066848f79af3f5421bc6013a6609f7f12f2
Dmitry Gontsa <dmitry.gontsa.invest@gmail.com>
2019-09-22 16:16:49 +0300

    New implementation added

--- Lesson_003/src/main/java/Task_8.java
+++ Lesson_003/src/main/java/Task_8.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
 
-public class Task_8
-{
-    public static void main(String[] args)
-    {
+public class Task_8 {
+    public static void main(String[] args) {
         int a = 72;
         int b = 101;
         int c = 108;
@@ -15,18 +13,19 @@ public class Task_8
         int i = 108;
         int j = 100;
         System.out.println(transformIntToChar(a, b, c, d, e, f, g, h, i, j));
+        printList(transformIntToChar(72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100));
     }
+
     private static ArrayList<Character> transformIntToChar(int... values) {
         ArrayList<Character> buffer = new ArrayList<Character>();
-        for (int i: values) {
+        for (int i : values) {
             buffer.add(((char) i));
         }
         return buffer;
     }
-    private static void printList(ArrayList<Character> buffer)
-    {
-        for (char c: buffer)
-        {
+
+    private static void printList(ArrayList<Character> buffer) {
+        for (char c : buffer) {
             System.out.print(c);
         }
     }
